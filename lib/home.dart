dba7617675cce6cfaa42dac642b680c0d2e3802d
wanbanka <hurt.stress@gmail.com>
2020-12-15 16:31:31 +0100

    TEST http_call_to_omdb_api

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,7 @@
-import 'dart:async';
+import 'dart:convert' as convert;
 import 'package:flutter/material.dart';
-import 'Components/CardActivity.dart';
 import './Components/Utilities/Colors.dart';
-import 'package:loadmore/loadmore.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -12,102 +11,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final List<dynamic> objetTest = [
-    {
-      "date": new DateTime(2021, 06, 27, 3),
-      "lieu": "HUELGOAT - 29690",
-      "type": "Marche",
-      "activite": "Trail",
-      "distance": 80,
-      "vitesse": 5.3,
-      "parcours": "Chemin",
-      "groupe": "UTMA 2021",
-      "participants": 2,
-      "organisateur": "Benjamin",
-      "coactivites": 0,
-      "recommandations": 0,
-      "description": "Prévoir 15 heures.",
-      "tags": ["Ultra-Trail", "Nature"],
-      "image":
-          "https://www.corunning.fr/avatar/5f612883e75c1d10ec38f9d6_activite.jpg",
-      "inscrit": false,
-      "distanciation": false,
-      "placesRestantes": 15
-    },
-    {
-      "date": new DateTime(2020, 12, 12, 9),
-      "lieu": "FRETERIVE - 73250",
-      "type": "Course",
-      "activite": "Course à pied",
-      "distance": 14,
-      "vitesse": 14.0,
-      "parcours": "Route",
-      "participants": 1,
-      "organisateur": "Lucien",
-      "coactivites": 0,
-      "recommandations": 0,
-      "description": "Prévoir 1 heure.",
-      "tags": ["Botanique", "Nature", "Yoga"],
-      "inscrit": true,
-      "distanciation": false,
-      "placesRestantes": 15
-    },
-    {
-      "date": new DateTime(2020, 11, 16, 13, 10),
-      "lieu": "Toulouse",
-      "type": "Marche",
-      "activite": "Marche à pied",
-      "distance": 10,
-      "vitesse": 10.0,
-      "parcours": "Route",
-      "groupe": "Toulouse st Cyprien",
-      "participants": 1,
-      "organisateur": "Emma",
-      "coactivites": 0,
-      "recommandations": 0,
-      "description": "Prévoir 1 heure.",
-      "tags": ["Balade", "Randonnée", "Echauffement"],
-      "inscrit": true,
-      "distanciation": false,
-      "placesRestantes": 15
-    }
-  ];
-
-  List<Widget> cards = [];
-
-  int get cardsLen => cards.length;
-
-  listCards() {
-    setState(() {
-      for (int index = 0; index < objetTest.length; index++) {
-        cards.add(CardActivity(
-          date: objetTest[index]['date'],
-          lieu: objetTest[index]['lieu'],
-          type: objetTest[index]['type'],
-          activite: objetTest[index]['activite'],
-          distance: objetTest[index]['distance'],
-          vitesse: objetTest[index]['vitesse'],
-          parcours: objetTest[index]['parcours'],
-          participants: objetTest[index]['participants'],
-          organisateur: objetTest[index]['organisateur'],
-          coactivites: objetTest[index]['coactivites'],
-          recommandations: objetTest[index]['recommandations'],
-          description: objetTest[index]['description'],
-          tags: objetTest[index]['tags'],
-          image: objetTest[index]['image'],
-          inscrit: objetTest[index]['inscrit'],
-          distanciation: objetTest[index]['distanciation'],
-          placesRestantes: objetTest[index]['placesRestantes'],
-        ));
-      }
-    });
-  }
+  final String url = 'http://www.omdbapi.com/?i=tt3896198&apikey=91cf00f8';
+  Map<String, dynamic> film;
+  bool dataOk = false;
 
   @override
   void initState() {
     super.initState();
+    recupFilm();
+  }
 
-    listCards();
+  Future<void> recupFilm() async {
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      film = convert.jsonDecode(response.body);
+
+      setState(() {
+        dataOk = !dataOk;
+      });
+    }
   }
 
   @override
@@ -119,164 +42,72 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(color: Colors.black),
         ),
         backgroundColor: Colors.white,
-        actions: <Widget>[IconButton(icon: Icon(Icons.menu), onPressed: null)],
       ),
-      body: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/background_home.png'),
-                fit: BoxFit.cover)),
-        child: RefreshIndicator(
-          child: LoadMore(
-            isFinish: cardsLen >= 4,
-            onLoadMore: loadMore,
-            child: ListView.builder(
-              itemBuilder: (BuildContext context, int i) {
-                return cards[i];
-              },
-              itemCount: cardsLen,
-            ),
-            whenEmptyLoad: false,
-            delegate: DefaultLoadMoreDelegate(),
-            textBuilder: DefaultLoadMoreTextBuilder.english,
-          ),
-          onRefresh: refresh,
-        ),
-      ),
+      body: dataOk ? affichage() : attente(),
+      backgroundColor: Colors.blueGrey[900],
       // ignore: missing_required_param
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add), backgroundColor: colorRed),
     );
   }
 
-  void load() {
-    print("load");
-
-    var newObjet = [
-      {
-        "date": new DateTime(2020, 11, 16, 13, 10),
-        "lieu": "Toulouse",
-        "type": "Marche",
-        "activite": "Marche à pied",
-        "distance": 10,
-        "vitesse": 10.0,
-        "parcours": "Route",
-        "groupe": "Toulouse st Cyprien",
-        "participants": 1,
-        "organisateur": "Emma",
-        "coactivites": 0,
-        "recommandations": 0,
-        "description": "Prévoir 1 heure.",
-        "tags": ["Balade", "Randonnée", "Echauffement"],
-        "inscrit": true,
-        "distanciation": false,
-        "placesRestantes": 15
-      },
-      {
-        "date": new DateTime(2020, 10, 31, 10),
-        "lieu": "Morlaix",
-        "type": "Velo",
-        "activite": "Vélo",
-        "distance": 10,
-        "vitesse": 4.0,
-        "parcours": "Chemin",
-        "groupe": "ASHM",
-        "participants": 1,
-        "organisateur": "Benjamin",
-        "coactivites": 3,
-        "recommandations": 0,
-        "description":
-            "Prévoir 2 heures 30 minutes. Octobre Rose organise pour la première fois à Morlaix une Marche Rose",
-        "tags": ["Balade"],
-        "inscrit": true,
-        "distanciation": false,
-        "placesRestantes": 15,
-        "image":
-            "https://www.corunning.fr/avatar/5f91a977d04392097db5c178_activite.jpg"
-      }
-    ];
-
-    setState(() {
-      for (int index = 0; index < newObjet.length; index++) {
-        cards.add(CardActivity(
-          date: newObjet[index]['date'],
-          lieu: newObjet[index]['lieu'],
-          type: newObjet[index]['type'],
-          activite: newObjet[index]['activite'],
-          distance: newObjet[index]['distance'],
-          vitesse: newObjet[index]['vitesse'],
-          parcours: newObjet[index]['parcours'],
-          participants: newObjet[index]['participants'],
-          organisateur: newObjet[index]['organisateur'],
-          coactivites: newObjet[index]['coactivites'],
-          recommandations: newObjet[index]['recommandations'],
-          description: newObjet[index]['description'],
-          tags: newObjet[index]['tags'],
-          image: newObjet[index]['image'],
-          inscrit: newObjet[index]['inscrit'],
-          distanciation: newObjet[index]['distanciation'],
-          placesRestantes: newObjet[index]['placesRestantes'],
-        ));
-      }
-      print("Data count : ${cards.length}");
-    });
+  Widget attente() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "En attente de données",
+            style: TextStyle(
+                color: colorWhite, fontSize: 20.0, fontWeight: FontWeight.bold),
+          ),
+          CircularProgressIndicator()
+        ],
+      ),
+    );
   }
 
-  Future<bool> loadMore() async {
-    print("On loadMore");
-    await Future.delayed(Duration(milliseconds: 2000));
-    load();
-    return true;
+  Widget affichage() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          film != null
+              ? Text(
+                  "${film['Title']}",
+                  style: TextStyle(
+                      color: colorWhite,
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold),
+                )
+              : Text("Aucune donnée"),
+          film != null
+              ? Text("${film['Year']}", style: TextStyle(color: colorWhite))
+              : Text("Aucune donnée"),
+          Padding(padding: const EdgeInsets.all(20.0)),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.6,
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(
+                  color: colorBlack,
+                  offset: Offset(0.0, 7.0),
+                  spreadRadius: 3.0,
+                  blurRadius: 15.0)
+            ]),
+            child: film != null
+                ? Image.network("${film['Poster']}")
+                : Text("Aucune donnée"),
+          ),
+          Padding(padding: const EdgeInsets.all(20.0)),
+          film != null
+              ? Text(
+                  "${film['Plot']}",
+                  style: TextStyle(color: colorWhite),
+                  textAlign: TextAlign.center,
+                )
+              : Text("Aucune donnée")
+        ],
+      ),
+    );
   }
-
-  Future<void> refresh() async {
-    await Future.delayed(Duration(milliseconds: 2000));
-    cards.clear();
-    listCards();
-  }
-
-  /* Put these buttons on the burger menu
-
-Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: RaisedButton.icon(
-                  onPressed: () {
-                    print('Raise your hands 2');
-                  },
-                  icon: Icon(
-                    Icons.add_box_outlined,
-                    color: const Color(0xffffffff),
-                  ),
-                  padding: const EdgeInsets.all(17),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0)),
-                  color: const Color(0xfffb3c00),
-                  label: Text(
-                    'Propose une activité, Théo',
-                    style: TextStyle(color: const Color(0xffffffff)),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: RaisedButton.icon(
-                  onPressed: () {
-                    print('Raise your hands');
-                  },
-                  icon: Icon(
-                    Icons.search,
-                    color: const Color(0xffffffff),
-                  ),
-                  padding: const EdgeInsets.all(17),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0)),
-                  color: const Color(0xff98c045),
-                  label: Text(
-                    'Recherche par activité, lieu',
-                    style: TextStyle(color: const Color(0xffffffff)),
-                  ),
-                ),
-              ),
-
-  */
 }
