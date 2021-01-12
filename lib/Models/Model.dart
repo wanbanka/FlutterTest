bf477a38f2e93a27ad55669fdba1238c507883d6
wanbanka <hurt.stress@gmail.com>
2021-01-12 14:22:15 +0100

    TEST mvc_flutter

--- lib/Models/Model.dart
+++ lib/Models/Model.dart
@@ -3,6 +3,6 @@ class Model {
   static int get counter => _counter;
   static int _counter = 0;
 
-  //Augmente la variable counter de 1
+  //Augmente la variable counter de 1 ++
   static int incrementCounter() => ++_counter;
 }
